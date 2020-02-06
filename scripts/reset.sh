42465dad7a1eb2f9bfecbf0bd59bc58100f53c5a
Rafael <rafael@geodb.com>
2020-02-06 08:30:50 +0100

    Added reset script Fixed permissions and volume mount to run fabric-ca in non-root mode
    Signed-off-by: Rafael <rafael@geodb.com>

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -5,8 +5,9 @@ reset(){
     printSection "Reset files and data"
     rm -rf ../volumes/fabric-ca-server/*
     checkFatalError $?
-    docker rm ${docker ps --filter "status=exited" --filter "name=fabric" -q}
-    docker volume rm --force fabric-openca-core-volume
+    docker rm $(docker ps --filter "status=exited" --filter "name=fabric" -q) &> /dev/null \
+        || echo "No docker containers have been found"
+    docker volume rm --force fabric-open-ca_fabric-openca-core-volume
     checkFatalError $?
 }
 
